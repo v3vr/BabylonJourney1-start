7d50606fb5582c8dd7bd83045771bee377459700
v3vr <71661816+v3vr@users.noreply.github.com>
2023-10-13 16:15:59 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {
+  Engine,
+  Scene,
+  Vector3,
+  SceneLoader,
+  ArcRotateCamera
+} from "@babylonjs/core";
+import "@babylonjs/loaders/glTF/2.0";
+import "@babylonjs/core/Helpers/sceneHelpers";
+import damagedHelmet from "../src/assets/DamagedHelmet.glb";
+import forest from "../src/assets/forest.env";
